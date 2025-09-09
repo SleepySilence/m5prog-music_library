79b0c68aeba6b6fb942f91cdcdcb5e825d8768db
ahmad <37993@ma-web.nl>
2025-09-09 18:12:09 +0200

    Les 1

---
+++ public/index.php
@@ -0,0 +1,28 @@
+<?php
+?><!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Music Library</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="assets/css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container py-5">
+      <header class="mb-4">
+        <h1 class="display-6">Music Library</h1>
+        <p class="text-muted m-0">Les 1</p>
+      </header>
+      <div class="album py-4 bg-light rounded">
+        <div class="row g-3">
+          <div class="col-12 text-center">Content of this project</div>
+        </div>
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+            crossorigin="anonymous"></script>
+  </body>
+</html>
